ba91ada40e019730c709f2f412b5074c1a5acf31
Su Anli <sual0001@gmail.com>
2021-06-28 12:35:13 +0800

    chore(package, android, ios): rename package

--- android/app/src/main/java/io/anli/boardgame/MainActivity.java
+++ android/app/src/main/java/io/anli/boardgame/MainActivity.java
@@ -1,4 +1,4 @@
-package com.template;
+package io.anli.boardgame;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Template";
+    return "BoardGame";
   }
 }
